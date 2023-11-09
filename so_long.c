69d7f35e18497ab58d1155a919ebd19fc45b4a02
Lorenzo Renzetti <lrenzett@e4r8p2.42roma.it>
2023-11-10 00:30:51 +0100

    fixed check

--- so_long.c
+++ so_long.c
@@ -6,26 +6,11 @@
 /*   By: lrenzett <lrenzett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 01:02:55 by lrenzett          #+#    #+#             */
-/*   Updated: 2023/11/10 00:03:25 by lrenzett         ###   ########.fr       */
+/*   Updated: 2023/11/10 00:30:07 by lrenzett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-/*int	handle_key(int keycode, t_game *game)
-{
-	if (keycode == 65307)
-		close_win(game);
-	if (keycode == 65362 || keycode == 119)
-		player_up(game);
-	if (keycode == 65364 || keycode == 115)
-		player_down(game);
-	if (keycode == 65361 || keycode == 97)
-		player_left(game);
-	if (keycode == 65363 || keycode == 100)
-		player_right(game);
-	return (0);
-}*/
 
 int main (int argc, char **argv)
 {
